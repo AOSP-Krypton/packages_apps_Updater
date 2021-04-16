f82160bb7e4c5c721daa7f966eb154a44ec47311
jhonboy121 <alfredmathew05@gmail.com>
2021-04-16 23:38:42 +0530

    Updater: improved apps functionality
    * added an empty settings page, will be populated later
    * added better contrasting themes for light and dark mode
    * main page now displays basic build info

---
+++ src/com/krypton/updater/Utils.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.krypton.updater;
+
+import android.content.res.Resources;
+import android.os.SystemProperties;
+
+import com.krypton.updater.R;
+
+public class Utils {
+
+    private static final String PROP_DEVICE = "ro.krypton.build.device";
+    private static final String PROP_VERSION = "ro.krypton.build.version";
+    private static final String PROP_FINGERPRINT = "ro.krypton.build.fingerprint";
+
+    public static String getDevice(Resources res) {
+        StringBuilder builder = new StringBuilder(res.getString(R.string.device_name_text));
+        builder.append(": ");
+        builder.append(SystemProperties.get(PROP_DEVICE, "unavailable"));
+        return builder.toString();
+    }
+
+    public static String getVersion(Resources res) {
+        StringBuilder builder = new StringBuilder(res.getString(R.string.version_text));
+        builder.append(": ");
+        builder.append(SystemProperties.get(PROP_VERSION, "unavailable"));
+        return builder.toString();
+    }
+
+    public static String getFingerprint(Resources res) {
+        StringBuilder builder = new StringBuilder(res.getString(R.string.fingerprint_text));
+        builder.append(": ");
+        builder.append(SystemProperties.get(PROP_FINGERPRINT, "unavailable"));
+        return builder.toString();
+    }
+
+}
--- src/com/krypton/updater/ui/UpdaterActivity.java
+++ src/com/krypton/updater/ui/UpdaterActivity.java
@@ -16,27 +16,50 @@
 
 package com.krypton.updater.ui;
 
-import android.app.Activity;
-import android.app.ActionBar;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.krypton.updater.R;
+import com.krypton.updater.Utils;
 
-public class UpdaterActivity extends Activity {
+public class UpdaterActivity extends AppCompatActivity {
+
+    private static final String TAG = "UpdaterActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
         setContentView(R.layout.updater_activity);
+        setBuildInfo();
+    }
+
+    private void setBuildInfo() {
+        Resources res = getResources();
+        ((TextView) findViewById(R.id.view_device)).setText(Utils.getDevice(res));
+        ((TextView) findViewById(R.id.view_version)).setText(Utils.getVersion(res));
+        ((TextView) findViewById(R.id.view_fingerprint)).setText(Utils.getFingerprint(res));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.settings_menu, menu);
+        return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
             finish();
+            return true;
+        } else if (item.getItemId() == R.id.option_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
