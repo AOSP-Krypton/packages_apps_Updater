9c62a564f75351cbcf73fdd6c95d7f30da7b2107
jhonboy121 <alfredmathew05@gmail.com>
2021-10-23 21:41:15 +0530

    Updater: rewrote di classes in kotlin
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>

---
+++ src/com/krypton/updater/di/UpdaterComponent.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.krypton.updater.di
+
+import com.krypton.updater.model.repos.*
+import com.krypton.updater.model.room.AppDatabase
+import com.krypton.updater.services.*
+import com.krypton.updater.ui.activity.UpdaterActivity
+import com.krypton.updater.ui.fragment.SettingsFragment
+import com.krypton.updater.viewmodel.DownloadViewModel
+import com.krypton.updater.workers.DownloadWorkerFactory
+
+import dagger.Component
+
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = arrayOf(UpdaterModule::class))
+interface UpdaterComponent {
+    fun getAppRepository(): AppRepository
+    fun getUpdateRepository(): UpdateRepository
+    fun getDownloadWorkerFactory(): DownloadWorkerFactory
+    fun inject(service: UpdateCheckerService)
+    fun inject(service: UpdateInstallerService)
+    fun inject(activity: UpdaterActivity)
+    fun inject(fragment: SettingsFragment)
+    fun inject(viewModel: DownloadViewModel)
+}
---
+++ src/com/krypton/updater/di/UpdaterModule.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2021 AOSP-Krypton Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.krypton.updater.di
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.UpdateEngine
+
+import androidx.annotation.NonNull
+import androidx.preference.PreferenceManager
+import androidx.room.Room
+import androidx.work.WorkManager
+
+import com.krypton.updater.model.room.AppDatabase
+import com.krypton.updater.util.Constants.DATABASE
+
+import dagger.Module
+import dagger.Provides
+
+import java.util.concurrent.Executors
+import java.util.concurrent.ExecutorService
+
+import javax.inject.Singleton
+
+@Module
+class UpdaterModule constructor(ctx: Context) {
+    val context = ctx
+    
+    @Provides
+    fun provideContext() = context
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(): AppDatabase = Room.databaseBuilder(
+        context, AppDatabase::class.java, DATABASE)
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideSharedPreferences(): SharedPreferences =
+        PreferenceManager.getDefaultSharedPreferences(context)
+
+    @Singleton
+    @Provides
+    fun provideUpdateEngine() = UpdateEngine()
+
+    @Singleton
+    @Provides
+    fun provideExecutorService(): ExecutorService = Executors.newCachedThreadPool()
+
+    @Singleton
+    @Provides
+    fun provideWorkManager() = WorkManager.getInstance(context)
+}
